5f513832ee662635066f58d9637f38c8ff01843f
Amidou Kante <kamdiou@gmail.com>
2023-09-23 20:02:27 -0400

    fix: merge conflicts

--- components/form.tsx
+++ components/form.tsx
@@ -53,8 +53,6 @@ export default function Form() {
   return (
     <form onSubmit={handleSubmit} className="w-2/5 flex flex-col place-items-center shadow-md rounded-3xl py-11 px-7 my-8 bg-gradient-to-r from-lime-200 via-yellow-200 to-purple-200">
       <h1 className="text-4xl mr-2 font-extrabold">Enter Cold wallet</h1>
-      <Avatar />
-      <WalletAddress />
       <Input
         placeholder="Enter ENS, lens, or wallet address"
         handleChange={handleChange}
